046627c84a9d5b8204937baeca1582f4d62a55dd
cannow <49660002+cannow@users.noreply.github.com>
2021-03-12 19:05:29 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace WhereMyAnagramsAt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(0, 0);
+            Point b = new Point(10, 10);
+
+            DistanceBetweenPoints(a, b);
+        }
+        public static double DistanceBetweenPoints(Point a, Point b)
+        {
+            return Math.Abs(Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2)));
+        }
+    }
+}
