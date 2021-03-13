8d34c34f4094bf50bc66bc0ef8c0347fc94a037e
cannow <49660002+cannow@users.noreply.github.com>
2021-03-13 12:39:59 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Drawing;
 
-namespace WhereMyAnagramsAt
+namespace MathFormulas
 {
     class Program
     {
@@ -10,11 +10,22 @@ namespace WhereMyAnagramsAt
             Point a = new Point(0, 0);
             Point b = new Point(10, 10);
 
+            int[] volumeA = {1, 2, 3};
+            int[] volumeB = {4, 5, 6};
+
             DistanceBetweenPoints(a, b);
+            FindDifference_Volume(volumeA, volumeB);
         }
         public static double DistanceBetweenPoints(Point a, Point b)
         {
+            //Console.WriteLine(Math.Abs(Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2))));
             return Math.Abs(Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2)));
+        }
+        
+        public static int FindDifference_Volume(int[] a, int[] b)
+        {
+            //Console.WriteLine(Math.Abs((a[0] * a[1] * a[2]) - (b[0] * b[1] * b[2])));
+            return Math.Abs((a[0] * a[1] * a[2]) - (b[0] * b[1] * b[2]));
         }
     }
 }
